f4404d1ed8c2d5e2bdac302fddd0acf4f93397b6
Ahmed El Hadad <Ahmedha95@outlook.com>
2025-08-10 21:12:55 +0300

    feat: implement autodesk viewer

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <section>
+    <h2 class="bg-grey-darken-4 pa-3 text-center">Vue Viewer</h2>
+
+    <div
+      class="pt-6"
+      style="
+        transform: scale(0.85);
+        transform-origin: top;
+        width: 100%;
+        height: 100vh;
+      "
+    >
+      <ApsViewer />
+    </div>
+  </section>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import ApsViewer from "./components/ApsViewer.vue";
+</script>
---
+++ src/components/ApsViewer.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div ref="viewerRef" style="width: 100%; height: 100vh"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const URN =
+  "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6eWt2N2lzd2NyNzNiZ21qZTVrd3p1eXAwbHAzODduYnphN3YzYzNqYTZheHBnMGFsLWJhc2ljLWFwcC9idWlsZGluZ18wNC5mYng";
+
+const viewerRef = ref(null);
+
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
+
+onMounted(async () => {
+  try {
+    const response = await fetch(`${API_URL}/api/auth/token`);
+    const { access_token } = await response.json();
+
+    const options = {
+      env: "AutodeskProduction",
+      accessToken: access_token,
+    };
+
+    Autodesk.Viewing.Initializer(options, () => {
+      const viewer = new Autodesk.Viewing.GuiViewer3D(viewerRef.value);
+      viewer.start();
+
+      Autodesk.Viewing.Document.load(
+        `urn:${URN}`,
+        (doc) => {
+          const defaultModel = doc.getRoot().getDefaultGeometry();
+          viewer.loadDocumentNode(doc, defaultModel);
+        },
+        (err) => {
+          console.error("Document load error:", err);
+        }
+      );
+    });
+  } catch (error) {
+    console.error("Error initializing Autodesk Viewer:", error);
+  }
+});
+</script>
